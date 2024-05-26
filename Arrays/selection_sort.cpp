b011a504646d7b5631f417a2ef6687a6c6f99103
KTKarthick006 <aswintamil@gmail.com>
2024-05-26 15:44:33 +0530

    Added selection sort

---
+++ Arrays/selection_sort.cpp
@@ -0,0 +1,43 @@
+# include<iostream>
+using namespace std;
+ 
+ void print_array(int array[],int n)
+ {
+    for (int i = 0; i < n; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+ }
+
+    int main ()
+    {   
+        cout<<"Enter the number of elements in the array : ";
+        int n ;
+        cin>>n;
+        int array[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>array[i];
+        }
+        cout<<"The array before sorting is : ";
+        print_array(array,n);
+
+        for (int i = 0; i < n-1; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                if (array[j]<array[i])
+                {
+                    int t = array[j];
+                    array[j]=array[i];
+                    array[i]=t;
+                }
+            }
+            
+        }
+        cout<<"The array after sorting is : ";
+        print_array(array,n); 
+        return 0;
+    }
+    
+ 
