105019dfc819fdafb267f24f1e9fa4b35b2f0fdf
KTKarthick006 <aswintamil@gmail.com>
2024-05-26 16:20:10 +0530

    made the code prettier

--- Arrays/bubble_sort.cpp
+++ Arrays/bubble_sort.cpp
@@ -1,38 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void print_array(int array[],int n)
+void print_array(int array[], int n)
 {
-    for (int  i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cout<<array[i]<<", ";
+        cout << array[i] << ", ";
     }
-    cout<<endl;
-    
+    cout << endl;
 }
 
-int main ()
+int main()
 {
-    int array[] = {10, 7, 9, 19, 8, 5, 4}; 
-    int n = (sizeof(array)/sizeof(array[0]));
-    cout<<"Array before sorting is : ";
-    print_array(array,n);
-    for (int  i = 0; i < n-1; i++)
+    int array[] = {10, 7, 9, 19, 8, 5, 4};
+    int n = (sizeof(array) / sizeof(array[0]));
+    cout << "Array before sorting is : ";
+    print_array(array, n);
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int  j = 0; j < n-i-1; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if (array[j]>array[j+1])
+            if (array[j] > array[j + 1])
             {
-                int t= array[j];
-                array[j]=array[j+1];
-                array[j+1]=t;
+                int t = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = t;
             }
-            
         }
-        
     }
-    cout<<"The sorted array is : ";
-    print_array(array,n);
+    cout << "The sorted array is : ";
+    print_array(array, n);
     return 0;
-    
 }
--- Arrays/selection_sort.cpp
+++ Arrays/selection_sort.cpp
@@ -1,43 +1,40 @@
-# include<iostream>
+#include <iostream>
 using namespace std;
- 
- void print_array(int array[],int n)
- {
+
+void print_array(int array[], int n)
+{
     for (int i = 0; i < n; i++)
     {
-        cout<<array[i]<<" ";
+        cout << array[i] << " ";
     }
- }
+}
 
-    int main ()
-    {   
-        cout<<"Enter the number of elements in the array : ";
-        int n ;
-        cin>>n;
-        int array[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin>>array[i];
-        }
-        cout<<"The array before sorting is : ";
-        print_array(array,n);
+int main()
+{
+    cout << "Enter the number of elements in the array : ";
+    int n;
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+    cout << "The array before sorting is : ";
+    print_array(array, n);
 
-        for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
         {
-            for (int j = i+1; j < n; j++)
+            if (array[j] < array[i])
             {
-                if (array[j]<array[i])
-                {
-                    int t = array[j];
-                    array[j]=array[i];
-                    array[i]=t;
-                }
+                int t = array[j];
+                array[j] = array[i];
+                array[i] = t;
             }
-            
         }
-        cout<<"The array after sorting is : ";
-        print_array(array,n); 
-        return 0;
     }
-    
- 
+    cout << "The array after sorting is : ";
+    print_array(array, n);
+    return 0;
+}
