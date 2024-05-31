f67c026a3ae730af4f1ed710b4bb59a6c0a0b986
KTKarthick006 <aswintamil@gmail.com>
2024-05-31 20:20:44 +0530

    Null ptr

---
+++ basics/null_ptr.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std ;
+
+int main ()
+{
+    int* ptr = NULL ;// null ptr does not has any value 
+    int* n_ptr = 0; // this is also a null ptr 
+    int* nu_ptr= nullptr; // best way 
+    return 0 ;
+}
