f7b4c4f5cfa3abfa3e8766d4346c986a0d3d75aa
KTKarthick006 <aswintamil@gmail.com>
2024-06-01 19:20:23 +0530

    Example for malloc

--- basics/dynamic_memory.cpp
+++ basics/dynamic_memory.cpp
@@ -3,9 +3,26 @@ using namespace std ;
 
 int main ()
 {
-    int* ptr =(int*) malloc(sizeof(int));
-    *ptr=10;
-    cout<<*ptr;
-    free(ptr);
-    return 0 ;
+    int i , n ;
+    cout<<"Enter the number of integers : ";
+    cin>>n;
+    int* ptr = (int*) malloc (n*sizeof(int));
+    if (ptr==nullptr)
+    {
+        cout<<"Menory not allocated ";
+        exit(1);
+    }
+    for (i = 0; i < n; i++)
+    {
+        cout<<"Enter an integer value : ";
+        cin>>*(ptr+i);
+    }
+
+    for ( i = 0; i < n; i++)
+    {
+        cout<<*(ptr+i)<<endl;
+    }
+    free (ptr);
+    
+    return 0;   
 }
