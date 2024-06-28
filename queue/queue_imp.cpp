968d6e76392e8b6d9fcce810b84009dec799865d
KTKarthick006 <aswintamil@gmail.com>
2024-06-28 10:15:47 +0530

    Add the driver code

--- queue/queue_imp.cpp
+++ queue/queue_imp.cpp
@@ -13,8 +13,13 @@ class queue
         int is_full () ;
         void peek () ;
         void enqueue (int x ) ;
-        void dequeue () ; 
+        //void dequeue () ; 
         void print_queue();
+    queue ()
+    {
+        front = -1 ;
+        rear = -1 ;
+    }
 };
 
 int queue :: is_empty ()
@@ -56,7 +61,7 @@ void queue :: print_queue ()
         return ;
     }
     
-    for (int i = 0; i < rear; i++)
+    for (int i = 0; i < rear+1; i++)
     {
         cout<<items[i]<<endl;
     }
@@ -72,4 +77,76 @@ void queue :: enqueue (int x )
     }
     
     items[++rear] = x;
+}
+
+int main ()
+{
+    class queue q ;
+    int ch , x ;
+    do
+    {
+        cout<<"Menu"<<endl;
+        cout<<"1-Is Empty "<<endl;
+        cout<<"2-Is full"<<endl;
+        cout<<"3-peek"<<endl;
+        cout<<"4-print"<<endl;
+        cout<<"5-Enqueue"<<endl;
+        cout<<"6-Dequeue "<<endl;
+        cout<<"7-Exit"<<endl;
+
+        cout<<"Enter your choice : ";
+        cin>>ch;
+        switch (ch)
+        {
+        case 1:
+            if(q.is_empty())
+            {
+                cout<<"The queue is empty"<<endl;
+            }
+            else
+            {
+                cout<<"The queue is not empty"<<endl;
+            }
+            
+            break;
+        
+        case 2:
+            if (q.is_full())
+            {
+                cout<<"The queue is full "<<endl;
+            }
+            else
+            {
+                cout<<"The queue is not full "<<endl;
+            }
+            
+            
+            break;
+
+        case 3:
+            q.peek();
+            break;
+        
+        case 4:
+            q.print_queue();
+            break;
+        
+        case 5:
+            int x ;
+            cout<<"Enter the element : ";
+            cin>>x;
+            q.enqueue(x);
+            break;
+        case 6:
+            //q.dequeue();
+            break;
+        case 7:
+            cout<<"Exit"<<endl;  
+            break;
+        default:
+            cout<<"Enter a valid choice"<<endl;
+            break;
+        }
+    } while (ch!=7);
+    return 0 ; 
 }
